93238921ef79bfea1a6bf4f84d7096e97f743562
Japleen Kaur <50547616+codermaan@users.noreply.github.com>
2020-05-02 14:44:02 +0530

    Add files via upload

---
+++ A3_1SecondLargestElement.java
@@ -0,0 +1,41 @@
+package array;
+
+import java.util.Scanner;
+
+public class A3_1SecondLargestElement {
+	public static void main (String[] args)
+	 {
+	     Scanner s= new Scanner(System.in);
+	     int t=s.nextInt();
+	     while(t-->0)
+	     {
+	         int n = s.nextInt();
+	         int a[] = new int[n];
+	         for(int i=0; i<n; i++)
+	         {
+	             a[i]= s.nextInt();
+	         }
+	         int max1=a[0];
+	         int max2=-1;
+	         for(int i=0; i<n; i++)
+	         {
+	             if(n==0)
+	             {
+	                 System.out.println("-1 ");
+	                 break;
+	             }
+	             if(a[i]>max1)
+	             {
+	                 max2= max1;
+	                 max1= a[i];
+	             }
+	             else if(a[i]>max2 && a[i]!=max1)
+	             {
+	            	 max2=a[i];
+	             }
+	         }
+	         System.out.println(max2);
+	     }
+	     s.close();
+	 }
+}
